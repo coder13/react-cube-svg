bbbb911cb7d11bf6b1e4c0d527f18a7288793598
Caleb Hoover <choover11@gmail.com>
2020-04-14 09:44:06 -0700

    initial rendering of just up face and abstracting out code

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-cube-svg'
+import { Cube } from 'react-cube-svg'
 import 'react-cube-svg/dist/index.css'
 
+const scramble = "D' F B L' B2 D F2 L U L2 D2 B2 L' B2 R' B2 L' D2 L F2 L"
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div>
+      <p>Should represent {scramble}</p>
+      <Cube scramble={scramble}/>
+    </div>
+  )
 }
 
 export default App
